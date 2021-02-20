6985418cede0c267df2176c2f52dfd06cc4c5e04
cabbaboi <71913806+cabbaboi@users.noreply.github.com>
2021-02-20 11:40:03 +0530

    Add files via upload

---
+++ QuizApp/index.js
@@ -0,0 +1,169 @@
+(function() {
+    var questions = [{
+      question: "Who is second mate of straw-hat pirates?",
+      choices: ['Luffy', 'Zoro', 'Law', 'God-Usopp', 'Nami'],
+      correctAnswer: 1
+    }, {
+      question: "What year was “JoJo’s Bizarre Adventure: Phantom Blood” first released??",
+      choices: [1987, 1947, 1931, 1917, 1861],
+      correctAnswer: 0
+    }, {
+      question: "Who created the animated movie “Spirited Away” (2001)??",
+      choices: ['Hayao Miyazaki', 'Makoto Shinkai', 'Masaki Yuasa', 'Hirohito Araki', 'Hiro Mashima'],
+      correctAnswer: 0
+    }, {
+      question: "Who is the main character with yellow hair in the anime Naruto??",
+      choices: ['Hatake Kakashi', 'Sasuke Uchiha', 'Hashirama Senzou', 'Kamikaze Minato', 'Uzumaki Naruto'],
+      correctAnswer: 4
+    }, {
+      question: "The anime Attack on Titan was directed by Tetsurō Araki, the same person who directed the anime?",
+      choices: ['My Hero Academia', 'Spirited Away', 'Highschool of the Dead', 'Black Clover', 'Your Name'],
+      correctAnswer: 2
+    }];
+    
+    var questionCounter = 0; //Tracks question number
+    var selections = []; //Array containing user choices
+    var quiz = $('#quiz'); //Quiz div object
+    
+    // Display initial question
+    displayNext();
+    
+    // Click handler for the 'next' button
+    $('#next').on('click', function (e) {
+      e.preventDefault();
+      
+      // Suspend click listener during fade animation
+      if(quiz.is(':animated')) {        
+        return false;
+      }
+      choose();
+      
+      // If no user selection, progress is stopped
+      if (isNaN(selections[questionCounter])) {
+        alert('Please make a selection!');
+      } else {
+        questionCounter++;
+        displayNext();
+      }
+    });
+    
+    // Click handler for the 'prev' button
+    $('#prev').on('click', function (e) {
+      e.preventDefault();
+      
+      if(quiz.is(':animated')) {
+        return false;
+      }
+      choose();
+      questionCounter--;
+      displayNext();
+    });
+    
+    // Click handler for the 'Start Over' button
+    $('#start').on('click', function (e) {
+      e.preventDefault();
+      
+      if(quiz.is(':animated')) {
+        return false;
+      }
+      questionCounter = 0;
+      selections = [];
+      displayNext();
+      $('#start').hide();
+    });
+    
+    // Animates buttons on hover
+    $('.button').on('mouseenter', function () {
+      $(this).addClass('active');
+    });
+    $('.button').on('mouseleave', function () {
+      $(this).removeClass('active');
+    });
+    
+    // Creates and returns the div that contains the questions and 
+    // the answer selections
+    function createQuestionElement(index) {
+      var qElement = $('<div>', {
+        id: 'question'
+      });
+      
+      var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+      qElement.append(header);
+      
+      var question = $('<p>').append(questions[index].question);
+      qElement.append(question);
+      
+      var radioButtons = createRadios(index);
+      qElement.append(radioButtons);
+      
+      return qElement;
+    }
+    
+    // Creates a list of the answer choices as radio inputs
+    function createRadios(index) {
+      var radioList = $('<ul>');
+      var item;
+      var input = '';
+      for (var i = 0; i < questions[index].choices.length; i++) {
+        item = $('<li>');
+        input = '<input type="radio" name="answer" value=' + i + ' />';
+        input += questions[index].choices[i];
+        item.append(input);
+        radioList.append(item);
+      }
+      return radioList;
+    }
+    
+    // Reads the user selection and pushes the value to an array
+    function choose() {
+      selections[questionCounter] = +$('input[name="answer"]:checked').val();
+    }
+    
+    // Displays next requested element
+    function displayNext() {
+      quiz.fadeOut(function() {
+        $('#question').remove();
+        
+        if(questionCounter < questions.length){
+          var nextQuestion = createQuestionElement(questionCounter);
+          quiz.append(nextQuestion).fadeIn();
+          if (!(isNaN(selections[questionCounter]))) {
+            $('input[value='+selections[questionCounter]+']').prop('checked', true);
+          }
+          
+          // Controls display of 'prev' button
+          if(questionCounter === 1){
+            $('#prev').show();
+          } else if(questionCounter === 0){
+            
+            $('#prev').hide();
+            $('#next').show();
+          }
+        }else {
+          var scoreElem = displayScore();
+          quiz.append(scoreElem).fadeIn();
+          $('#next').hide();
+          $('#prev').hide();
+          $('#start').show();
+        }
+      });
+    }
+    
+    // Computes score and returns a paragraph element to be displayed
+    function displayScore() {
+      var score = $('<p>',{id: 'question'});
+      
+      var numCorrect = 0;
+      for (var i = 0; i < selections.length; i++) {
+        if (selections[i] === questions[i].correctAnswer) {
+          numCorrect++;
+        }
+      }
+      if(numCorrect==questions.length){
+        score.append('congratulations!! ');
+      }
+      score.append('You got ' + numCorrect + ' answers out of ' +
+                   questions.length + ' right!!!');
+      return score;
+    }
+  })();
